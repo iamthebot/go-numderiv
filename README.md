f00410c183787e0a6becb67eb580fdd66aa01498
Alfredo Luque <alfredo@blacklab.capital>
2015-08-24 23:48:31 +0000

    push readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# go-differentiate
-numerical differentiation in golang using finite differences
+##Introduction
+Numderiv implements classic finite-difference numerical differentiation for the calculation of first, second, or third order derivatives on a slice of data. Functions for both first-order and second-order approximations are provided.
+
+A future update to this package will provide numerical differentiation via spline fitting (helpful when dealing with irregular time series).
+
+
+##Usage
+See the godoc documentation [here](http://godoc.org/github.com/iamthebot/go-numderiv)
--- derive.go
+++ derive.go
@@ -4,10 +4,12 @@ import (
 	"errors"
 )
 
-//Calculate a backwards 3rd order approximate 2nd derivative of an int slice
-//d: data
-//x: index in data
-//s: step size
+/*
+* NumDeriv implements functions for simple differentiation of uniformly sampled time-series
+* Provided are first and second order approximate derivative calculations using the finite-difference method
+ */
+
+//Calculate a backwards 2nd order approximate 2nd derivative of an int slice
 func BackSecondDerivInt(d []int, x int, s int) (float64, error) {
 	if (x - 3*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -19,10 +21,7 @@ func BackSecondDerivInt(d []int, x int, s int) (float64, error) {
 	return ((2.0*float64(d[x]) - 5.0*float64(d[x-s]) + 4.0*float64(d[x-2*s]) - float64(d[x-3.0*s])) / (float64(s) * float64(s))), nil
 }
 
-//Calculate a backwards 3rd order approximate 2nd derivative of an int32 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards 2nd order approximate 2nd derivative of an int32 slice
 func BackSecondDerivInt32(d []int32, x int32, s int32) (float64, error) {
 	if (x - 3*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -34,10 +33,7 @@ func BackSecondDerivInt32(d []int32, x int32, s int32) (float64, error) {
 	return ((2.0*float64(d[x]) - 5.0*float64(d[x-s]) + 4.0*float64(d[x-2*s]) - float64(d[x-3.0*s])) / (float64(s) * float64(s))), nil
 }
 
-//Calculate a backwards 3rd order approximate 2nd derivative of an int64 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards 2nd order approximate 2nd derivative of an int64 slice
 func BackSecondDerivInt64(d []int64, x int64, s int64) (float64, error) {
 	if (x - 3*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -49,10 +45,7 @@ func BackSecondDerivInt64(d []int64, x int64, s int64) (float64, error) {
 	return ((2.0*float64(d[x]) - 5.0*float64(d[x-s]) + 4.0*float64(d[x-2*s]) - float64(d[x-3.0*s])) / (float64(s) * float64(s))), nil
 }
 
-//Calculate a backwards 3rd order approximate 2nd derivative of a float32 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards 2nd order approximate 2nd derivative of a float32 slice
 func BackSecondDerivFloat32(d []float32, x int, s int) (float64, error) {
 	if (x - 3*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -64,10 +57,7 @@ func BackSecondDerivFloat32(d []float32, x int, s int) (float64, error) {
 	return ((2.0*float64(d[x]) - 5.0*float64(d[x-s]) + 4.0*float64(d[x-2*s]) - float64(d[x-3.0*s])) / (float64(s) * float64(s))), nil
 }
 
-//Calculate a backwards 3rd order approximate 2nd derivative of a float64 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards 2nd order approximate 2nd derivative of a float64 slice
 func BackSecondDerivFloat64(d []float64, x int, s int) (float64, error) {
 	if (x - 3*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -79,10 +69,7 @@ func BackSecondDerivFloat64(d []float64, x int, s int) (float64, error) {
 	return ((2.0*d[x] - 5.0*d[x-s] + 4.0*d[x-2*s] - d[x-3.0*s]) / (float64(s) * float64(s))), nil
 }
 
-//Calculate a backwards looking 3rd order approximate 1st derivative of an int slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards looking 2nd order approximate 1st derivative of an int slice
 func BackFirstDerivInt(d []int, x int, s int) (float64, error) {
 	if (x - 2*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -94,10 +81,7 @@ func BackFirstDerivInt(d []int, x int, s int) (float64, error) {
 	return ((3.0*float64(d[x]) - 4.0*float64(d[x-s]) - float64(d[x-2*s])) / (2.0 * float64(s))), nil
 }
 
-//Calculate a backwards looking 3rd order approximate 1st derivative of an int32 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards looking 2nd order approximate 1st derivative of an int32 slice
 func BackFirstDerivInt32(d []int32, x int32, s int32) (float64, error) {
 	if (x - 2*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -109,10 +93,7 @@ func BackFirstDerivInt32(d []int32, x int32, s int32) (float64, error) {
 	return ((3.0*float64(d[x]) - 4.0*float64(d[x-s]) - float64(d[x-2*s])) / (2.0 * float64(s))), nil
 }
 
-//Calculate a backwards looking 3rd order approximate 1st derivative of an int64 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards looking 2nd order approximate 1st derivative of an int64 slice
 func BackFirstDerivInt64(d []int64, x int64, s int64) (float64, error) {
 	if (x - 2*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -124,10 +105,7 @@ func BackFirstDerivInt64(d []int64, x int64, s int64) (float64, error) {
 	return ((3.0*float64(d[x]) - 4.0*float64(d[x-s]) - float64(d[x-2*s])) / (2.0 * float64(s))), nil
 }
 
-//Calculate a backwards looking 3rd order approximate 1st derivative of a float32 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards looking 2nd order approximate 1st derivative of a float32 slice
 func BackFirstDerivFloat32(d []float32, x int, s int) (float64, error) {
 	if (x - 2*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
@@ -139,10 +117,7 @@ func BackFirstDerivFloat32(d []float32, x int, s int) (float64, error) {
 	return ((3.0*float64(d[x]) - 4.0*float64(d[x-s]) - float64(d[x-2*s])) / (2.0 * float64(s))), nil
 }
 
-//Calculate a backwards looking 3rd order approximate 1st derivative of a float64 slice
-//d: data
-//x: index in data
-//s: step size
+//Calculate a backwards looking 2nd order approximate 1st derivative of a float64 slice
 func BackFirstDerivFloat64(d []float64, x int, s int) (float64, error) {
 	if (x - 2*s) < 0 {
 		return 0.0, errors.New("step size puts desired point out of bounds")
